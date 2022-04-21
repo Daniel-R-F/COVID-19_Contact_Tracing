929fccb4c3ef3df38f0692d2af9ed4ed480dc307
jonny831 <64038722+jonny831@users.noreply.github.com>
2022-04-20 17:07:12 -0700

    getFromLocation

--- app/src/main/java/com/example/contacttracing/LocationLogging.java
+++ app/src/main/java/com/example/contacttracing/LocationLogging.java
@@ -6,9 +6,12 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.ViewDebug;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,6 +23,10 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 /*
     @todo - The service should run all the time. Not just when app is in focus.
@@ -50,8 +57,54 @@ public class LocationLogging extends Service {
             public void onLocationResult(@NonNull LocationResult locationResult) {
                 super.onLocationResult(locationResult);
                 Log.d(TAG, "onLocationResult: Location Update!");
-                updateUI(locationResult.getLastLocation());
+                Location location = locationResult.getLastLocation();
+                updateUI(location);
+                getFromLocation(location);
+
             }
+
+
+
+//            Geocoder geocoder = new Geocoder(LocationLogging.this) {
+//                @Override
+//                public List<Address> getFromLocation(@NonNull double latitude, double longitude, int maxResults) throws IOException {
+//
+//                    try {
+//                        List<Address> addresses = getFromLocation(latitude, longitude, maxResults);
+//                        Log.d(TAG, Double.toString(latitude) + " " + Double.toString(longitude));
+//                        return addresses;
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+////                    return super.getFromLocation(latitude, longitude, maxResults);
+////                    return addresses;
+//
+//            }
+
+private String getFromLocation(Location location) {
+    String strAdd = "";
+    Geocoder geocoder = new Geocoder(LocationLogging.this, Locale.getDefault());
+    try {
+        List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(),1);
+        if (addresses != null) {
+            Address returnedAddress = addresses.get(0);
+            StringBuilder strReturnedAddress = new StringBuilder("");
+
+            for (int i = 0; i <= returnedAddress.getMaxAddressLineIndex(); i++) {
+                strReturnedAddress.append(returnedAddress.getAddressLine(i)).append("\n");
+            }
+            strAdd = strReturnedAddress.toString();
+            Log.d("My Current location address", strReturnedAddress.toString());
+        } else {
+            Log.d("My Current location address", "No Address returned!");
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+        Log.d("My Current location address", "Canont get Address!");
+    }
+    return strAdd;
+}
+
 
             @Override
             public void onLocationAvailability(@NonNull LocationAvailability locationAvailability) {
@@ -61,6 +114,7 @@ public class LocationLogging extends Service {
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+
 
  // USED FOR A ONE TIME UPDATE
 //        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
